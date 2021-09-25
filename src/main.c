3903565edc5163f09eeb370805dbcf344ff02ce8
louisnfr <louis.raffin@gmail.com>
2021-09-25 14:12:05 +0200

    minishell

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: lraffin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/25 13:36:22 by lraffin           #+#    #+#             */
-/*   Updated: 2021/09/25 14:09:31 by lraffin          ###   ########.fr       */
+/*   Updated: 2021/09/25 14:12:02 by lraffin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,6 @@
 void	prompt(void)
 {
 	printf("\033[0mminishell\033[1;32m$\033[0m> ");
-}
-
-int	mygetch(void)
-{
-	int ch;
-	struct termios oldt, newt;
-
-	tcgetattr(STDIN_FILENO, &oldt);
-	newt = oldt;
-	newt.c_lflag &= ~(ICANON | ECHO);
-	tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-	ch = getchar();
-	tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-	return (ch);
 }
 
 int	main(int ac, char **av, char **envp)
