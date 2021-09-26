69fedccae038c7077ba86e3c5f1446c079b5332e
louisnfr <louis.raffin@gmail.com>
2021-09-27 01:21:30 +0200

    minishell

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: lraffin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/25 13:36:22 by lraffin           #+#    #+#             */
-/*   Updated: 2021/09/27 01:13:06 by lraffin          ###   ########.fr       */
+/*   Updated: 2021/09/27 01:20:50 by lraffin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,15 @@ int	main(int ac, char **av, char **envp)
 		exit(EXIT_FAILURE);
 	while (1)
 	{
+		// readline LEAKS FUCK 
 		input = readline("minishell\033[1;32m$\033[0m ");
 		if (!ft_strcmp(input, "exit"))
+		{
+			free(input);
 			exit(EXIT_SUCCESS);
+		}
 		printf("cmd: %s\n", input);
+		free(input);
 	}
 	return (0);
 }
