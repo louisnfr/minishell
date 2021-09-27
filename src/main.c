3e3b2eb068f3cd0c17022b3009bdadf789485728
louisnfr <louis.raffin@gmail.com>
2021-09-27 20:24:24 +0200

    minishell

--- src/main.c
+++ src/main.c
@@ -6,25 +6,18 @@
 /*   By: lraffin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/25 13:36:22 by lraffin           #+#    #+#             */
-/*   Updated: 2021/09/27 20:15:25 by lraffin          ###   ########.fr       */
+/*   Updated: 2021/09/27 20:24:15 by lraffin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+https://misc.flogisoft.com/bash/tip_colors_and_formatting
+
 void	prompt(char **env)
 {
+	(void)env;
 	ft_putstr_fd("minishell\033[1;32m$\033[0m ", 1);
-}
-
-void	signal_handler(int signo)
-{
-	if (signo == SIGINT)
-	{
-		prompt();
-		// ft_putstr("\n");
-		signal(SIGINT, signal_handler);
-	}
 }
 
 void	check(char *input)
@@ -38,13 +31,14 @@ int	main(int ac, char **av, char **env)
 	(void)av;
 	char	*input;
 
+	for(int q=0; env[q]; q++)
+		printf("env[%d]: %s\n", q, env[q]);
 	if (ac != 1)
 		exit(EXIT_FAILURE);
 	while (1)
 	{
 		prompt(env);
 		get_next_line(0, &input);
-		// signal(SIGINT, signal_handler);
 		check(input);
 	}
 	return (0);
