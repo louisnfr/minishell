a2a9540188db64bf462e5f8f45643b61e15653ae
louisnfr <louis.raffin@gmail.com>
2021-10-01 21:13:46 +0200

    removed termios

--- src/main.c
+++ src/main.c
@@ -6,46 +6,11 @@
 /*   By: lraffin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/25 13:36:22 by lraffin           #+#    #+#             */
-/*   Updated: 2021/10/01 19:58:29 by lraffin          ###   ########.fr       */
+/*   Updated: 2021/10/01 21:11:05 by lraffin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-struct termios tty_attr_old;
-struct termios tty_attr;
-int old_keyboard_mode;
-
-int init_terminal()
-{
-	/* init terminal device to raw mode */
-	if (ioctl(0, KDGKBMODE, &old_keyboard_mode) < 0)
-	{
-		/* this usually happens when X is running */
-		/* Print some error and exit              */
-		printf("error\n");
-		exit(EXIT_FAILURE);
-	}
-	tcgetattr(0, &tty_attr_old);
-	tty_attr = tty_attr_old;
-	/* turn off buffering, echo and key processing */
-	tty_attr.c_lflag &= ~(ICANON | ECHO | ISIG);
-	tty_attr.c_iflag &= ~(ISTRIP | INLCR | ICRNL |
-						  IGNCR | IXON | IXOFF);
-	/* block until 1 key comes in */
-	tty_attr.c_cc[VMIN] = 1;
-	tcsetattr(0, TCSANOW, &tty_attr);
-	ioctl(0, KDSKBMODE, K_RAW);
-	printf("check\n");
-	return 1;
-}
-
-void exit_gracefully(int stat)
-{
-	tcsetattr(0, TCSAFLUSH, &tty_attr_old);
-	ioctl(0, KDSKBMODE, old_keyboard_mode);
-	exit(stat);
-}
 
 t_data *init_data(char **envp)
 {
@@ -79,8 +44,6 @@ int main(int ac, char **av, char **envp)
 	t_data *data;
 	char *input;
 
-	// init_terminal();
-	// exit_gracefully(0);
 	(void)av;
 	if (ac != 1)
 		exit(EXIT_FAILURE);
