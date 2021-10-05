53383d54ff4d2420d5ac702dc7283196c2c5c21a
Louis RAFFIN <lraffin@e3r4p8.42.fr>
2021-10-05 15:15:57 +0200

    prompt leaks fix

--- src/shell/display.c
+++ src/shell/display.c
@@ -6,7 +6,7 @@
 /*   By: lraffin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/28 15:06:40 by lraffin           #+#    #+#             */
-/*   Updated: 2021/10/04 17:04:24 by lraffin          ###   ########.fr       */
+/*   Updated: 2021/10/05 15:15:22 by lraffin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,12 +27,18 @@ char	*prompt(void)
 		cwd = ft_strjoin("~", tmp);
 		free(tmp);
 	}
-	usr = ft_strjoin("\e[32;1m", usr);
-	usr = ft_strjoin(usr, "\e[0m:");
-	usr = ft_strjoin(usr, "\e[36;1m");
-	usr = ft_strjoin(usr, cwd);
-	usr = ft_strjoin(usr, "$ \e[0m");
-	return (usr);
+	tmp = ft_strjoin("\e[32;1m", usr);
+	free(usr);
+	usr = ft_strjoin(tmp, "\e[0m:");
+	free(tmp);
+	tmp = ft_strjoin(usr, "\e[36;1m");
+	free(usr);
+	usr = ft_strjoin(tmp, cwd);
+	free(tmp);
+	free(cwd);
+	cwd = ft_strjoin(usr, "$ \e[0m");
+	free(usr);
+	return (cwd);
 }
 
 /* "\e[32;1m[usr]\e[0m:\e[36;1m[cwd]$ \e[0m" */
