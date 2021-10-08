2811043da51b82db9260a8813be91d4b9c3a7434
louisnfr <louis.raffin@gmail.com>
2021-10-08 18:51:00 +0200

    fixing export

--- src/builtin/export.c
+++ src/builtin/export.c
@@ -6,11 +6,44 @@
 /*   By: lraffin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/28 16:27:04 by lraffin           #+#    #+#             */
-/*   Updated: 2021/10/05 15:02:04 by lraffin          ###   ########.fr       */
+/*   Updated: 2021/10/08 18:49:17 by lraffin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+t_bool	already_exists(char *var, t_data *data)
+{
+	t_env	*tmp;
+
+	tmp = data->env;
+	while (tmp)
+	{
+		if (!ft_strcmp(tmp->key, var))
+			return (TRUE);
+		tmp = tmp->next;
+	}
+	return (FALSE);
+}
+
+void	print_export(t_data *data)
+{
+	t_env	*tmp;
+
+	tmp = data->env;
+	while (tmp)
+	{
+		ft_putstr("export ");
+		ft_putstr(tmp->key);
+		if (tmp->value)
+		{
+			ft_putchar('=');
+			ft_putstr(tmp->value);
+		}
+		ft_putchar('\n');
+		tmp = tmp->next;
+	}
+}
 
 t_bool	exec_export(t_cmd *cmd_list, t_data *data)
 {
@@ -18,10 +51,15 @@ t_bool	exec_export(t_cmd *cmd_list, t_data *data)
 	int		i;
 
 	i = -1;
-	while (cmd_list->args[++i])
+	if (!cmd_list->args)
+		print_export(data);
+	while (cmd_list->args && cmd_list->args[++i])
 	{
 		var = ft_split_on_first(cmd_list->args[i], '=');
-		add_var(&data->env, new_var(var[0], var[1]));
+		if (!already_exists(var[0], data))
+			add_var(&data->env, new_var(var[0], var[1]));
+		else
+			printf("existe deja\n");
 		free_split(var);
 	}
 	return (EXIT_SUCCESS);
