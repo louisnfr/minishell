ad19c6cd17083ae1447d45b77bf46110f079234d
Louis Raffin <louis.raffin@gmail.com>
2021-11-17 02:55:17 +0100

    minishell

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: lraffin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 14:32:15 by efrancon          #+#    #+#             */
-/*   Updated: 2021/11/17 02:45:59 by lraffin          ###   ########.fr       */
+/*   Updated: 2021/11/17 02:54:18 by lraffin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,6 @@ int	main(int ac, char **av, char **envp)
 {
 	t_data		*data;
 	char		*input;
-
-	char *s = "1234?";
-	printf("ret: %d\n", str_is_digit(s));
-	exit(0);
 
 	if (ac != 1 || av[1])
 		exit(EXIT_FAILURE);
