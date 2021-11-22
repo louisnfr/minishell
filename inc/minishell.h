d4163d7fd5efb9fc5824afe3b8d73430d49321e0
Louis Raffin <louis.raffin@gmail.com>
2021-11-22 12:40:33 +0100

    .h added

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,7 +6,7 @@
 /*   By: lraffin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/25 13:37:00 by lraffin           #+#    #+#             */
-/*   Updated: 2021/11/22 12:06:56 by lraffin          ###   ########.fr       */
+/*   Updated: 2021/11/22 12:40:05 by lraffin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,10 @@
 # include "libft.h"
 # include "enum.h"
 # include "struct.h"
+# include "builtins.h"
+# include "shell.h"
+# include "parsing.h"
+# include "env.h"
 
 # ifdef __linux__
 #  include <linux/kd.h>
@@ -64,47 +68,9 @@ t_bool		safe_unlink(char *pathname, t_data *data);
 t_bool		safe_close_fd(int fd, t_data *data);
 char		**safe_double_strdup(char **str, int size, t_data *data);
 /*
-** builtin
-*/
-t_bool		exec_echo(t_cmd *cmd_list, t_data *data);
-t_bool		exec_cd(t_cmd *cmd_list, t_data *data);
-int			get_ret(char *oldpwd, char *cdpath, t_cmd *cmd_list, t_data *data);
-t_bool		ch_dir(char *dir, char *error_msg, t_cmd *cmd_list);
-t_bool		exec_pwd(t_cmd *cmd_list, t_data *data);
-t_bool		exec_env(t_cmd *cmd_list, t_data *data);
-t_bool		exec_history(t_cmd *cmd_list, t_data *data);
-t_bool		exec_export(t_cmd *cmd_list, t_data *data);
-t_bool		exec_unset(t_cmd *cmd_list, t_data *data);
-t_bool		exec_exit(t_cmd *cmd_list, t_data *data);
-/*
-** create_env
-*/
-char		*prompt(t_data *data);
-void		display_error_message(char *cmd, char *errno_msg, int fd);
-void		syntax_error_char_msg(int nb, char token);
-char		**syntax_error_str_msg(char *token, char **str);
-/*
-** shell/env
-*/
-t_env		*find_prev_var(char *key, t_env *env);
-void		add_var_env(t_data *data, t_cmd *cmd_list, int i);
-void		create_update_env(char **envp, t_data *data);
-int			get_env_size(t_env *env);
-char		**env_to_char(t_env *env, t_data *data);
-t_env		*dup_env(char **envp, t_data *data);
-void		update_env(t_data *data, t_env *env);
-char		*get_env(char *key, t_env *env);
-void		set_env(char *key, char *new_value, t_env *env, t_data *data);
-t_env		*new_var(char *name, char *value, int is_value);
-t_env		*get_last(t_env *g_env);
-void		add_var(t_env **g_env, t_env *new_var);
-void		print_env(t_env *g_env, t_cmd *cmd_list);
-void		print_export(t_cmd *cmd_list, t_data *data);
-/*
 ** shell/exec
 */
 int			exec(t_data *data);
-t_bool		exec_builtin(t_cmd *cmd_list, t_data *data);
 void		handle_bin_command(int *exit_code, t_cmd **cmd_list, t_data *data);
 t_bool		exec_bin_command(pid_t *pid, t_cmd *cmd_list, t_data *data);
 t_bool		error_bin_cmd(
@@ -131,219 +97,12 @@ void		close_pipe(t_cmd **cmd, t_data *data);
 void		close_other_pipes(t_cmd **cmd, t_data *data);
 void		close_all_pipes(t_cmd **cmd, t_data *data);
 t_bool		create_fork(int i, pid_t *pid, t_data *data);
-/*
-** parsing/utils
-*/
-char		**get_paths(t_data *data);
-void		setup_cmd_list(t_cmd *cmd_list);
-void		clean_cmd_list(t_cmd **cmd_list, t_data *data);
-void		create_new_cmd(t_cmd **cmd_list, t_data *data);
-void		print_list(t_cmd *cmd_list);
-/*
-** parsing/parser
-*/
-t_bool		parse(char *input, t_data *data);
-void		handle_bin_cmd(
-				int delimiter, char **argv, t_cmd *cmd_list, t_data *data);
-void		handle_builtin_cmd(
-				int delimiter, char **argv, t_cmd *cmd_list, t_data *data);
-char		*find_cmd_path(
-				char *command, char *path, char **all_paths, t_data *data);
-char		**find_cmd_options(char **argv, t_data *data);
-char		**find_cmd_options_end(char **argv, t_data *data);
-char		**find_cmd_args(char **argv, t_data *data);
-t_bool		cmd_is_builtin(char *cmd);
-t_bool		is_delimiter(char *str);
-int			get_delimiter(char *str);
-t_bool		is_redirection(char *str);
-t_bool		is_parenthese(char *str);
-int			get_redirection(char *str);
-void		parse_redirections(char **argv, t_cmd *cmd_list, t_data *data);
-void		display_error_redir(int fd, char *filename, char *errno_msg);
-void		parse_pipes(t_cmd *cmd_list, t_data *data);
-void		parse_special_value(t_cmd *cmd_list, t_data *data);
-void		handle_parentheses(int delimiter, char **argv, t_data *data);
-void		handle_bin_cmd(
-				int delimiter, char **argv, t_cmd *cmd_list, t_data *data);
-void		handle_builtin_cmd(
-				int delimiter, char **argv, t_cmd *cmd_list, t_data *data);
-void		free_fd_array(int size, int **fd_array);
-void		close_cmd_pipes_fd(t_cmd **cmd_list, t_data *data);
-void		parse_redirections_heredoc(
-				char **argv, t_cmd *cmd_list, t_data *data);
-/*
-** parsing/lexer
-*/
-char		*upgrade_input(char *input, t_data *data);
-char		*parse_env_variable(char *input, t_data *data);
-int			is_charset_env(char c);
-char		*get_env_val(char *str, int *i, t_data *data);
-char		*get_env_key(char *str, int *i, t_data *data);
-int			get_length_env_value(
-				int double_quotes, char *env_key, t_data *data);
-int			get_length_new_input(char *str, t_data *data);
-char		*transform_str(
-				char *str, char *pid_value, char *ret_value, t_data *data);
-char		*transform_pid_value(char *str, char *value, t_data *data);
-char		*transform_ret_value(char *str, char *value, t_data *data);
-void		handle_env_variable(
-				int double_quotes, t_var *var, char *str, t_data *data);
-void		fill_env_value(
-				char *new_str, t_var *var, char *str, t_data *data);
-void		fill_with_value(t_var *var, char *new_str, char *value);
-t_var		*init_var(t_data *data);
-void		free_var(t_var *var);
-int			setup_variables(int *i, int *count, int *double_quotes, char *str);
-void		increment_var(int *i, int *length);
-void		increment_i_j(t_var *var);
-/*
-** parsing/split
-*/
-char		**split_input(char *str, t_data *data);
-int			split_count_words(char *str, t_data *data);
-int			is_charset_split(char c);
-int			is_delimiter_split(char c);
-void		count_in_quotes_split(int *j, char *tmp);
-void		fill_quotes_split(char **str, char **strs, int *i, int *j);
-/*
-** parsing/checker
-*/
-char		*check_input(char *input, t_data *data);
-void		*display_error_msg_simple_dot(char **str);
-int			check_delimiter(
-				char *str, char delimiter, int *words, t_data *data);
-int			check_error_delimiter(
-				int j, char *str, int delimiter, t_data *data);
-int			check_multiple_delimiters(
-				char *str, int j, t_data *data);
-int			display_error_msg_delimiter(int count, char delimiter);
-t_bool		check_beginning(char *str, int i);
-t_bool		check_error_redir(char *str, int *i);
-int			is_error_redir(char *str, int j);
-int			check_right_marks(char *str, int j);
-int			check_left_marks(char *str, int j);
-char		**check_argv(char **argv, t_data *data);
-t_bool		check_unclosed_quotes(char *input);
-t_bool		check_unclosed_parentheses(char *input);
-/*
-** parsing/heredoc
-*/
-t_bool		process_ctrl_key_hd(
-				t_data *data, t_config *sh, char *delimiter, int c);
-char		*heredoc_process_keypress(
-				t_data *data, t_config *sh, t_history *hist, char *delimiter);
-t_bool		process_ctrl_d_heredoc(
-				t_data *data, t_config *sh, char *delimiter);
-void		process_ctrl_l_heredoc(t_config *sh);
-t_bool		process_ctrl_c_heredoc(t_config *sh);
-char		*heredoc_shell(
-				t_data *data, t_config *sh, t_history *hist, char *delimiter);
-t_bool		read_heredoc(t_bool quotes, t_cmd *cmd_list, t_data *data);
-char		*heredoc_env_variable(char *input, t_data *data);
-char		*heredoc_special_value(
-				char *str, char *value, char character, t_data *data);
-t_bool		handle_heredoc_quotes(
-				int *i, char **str, char **strs, t_data *data);
-/*
-** exit
-*/
-void		free_env(t_env *g_env);
-void		free_split(char **args);
-t_bool		cd_error(t_cmd *cmd_list);
-t_bool		cd_error_msg(char *s, t_cmd *cmd_list);
-t_bool		check_export(char *s);
-int			export_error(t_cmd *cmd_list, int i);
-
-/*** shell ***/
-
-void		update_input(t_config *sh, t_history *hist, int c);
-char		*send_cmd(t_config *sh);
-
-/*** raw ***/
-
-void		disable_raw_mode(t_config *sh);
-t_bool		enable_raw_mode(t_config *sh);
-void		enable_heredoc_raw_mode(t_config *sh);
-void		init_shell(t_config *sh);
-
-/*** insert_char ***/
-
-void		insert_char_history(t_history *hist, int cx, char c, int search);
-char		*insert_char(char *current, int cx, char c);
-
-/*** erase_char ***/
-
-void		erase_char(char *s, int cx);
-void		erase_char_history(t_history *hist, int cx, int search);
-void		erase_cmd_history(char *input, t_history *history, int search);
-
-/*** erase_keys ***/
-
-void		erase_char_current(t_config *sh);
-void		erase_char_input(t_config *sh, t_history *hist);
-void		process_del_key(t_config *sh, t_history *hist);
-void		process_backspace_key(t_config *sh, t_history *hist);
-
-/*** read ***/
-
-int			read_key(t_config *sh);
-int			read_escape_seq(void);
-int			read_arrow_key(char c);
-
-/*** tab ***/
-
-void		process_tab_key(t_config *sh);
-
-/*** arrows ***/
-
-void		process_arrow_key(t_config *sh, t_history *hist, int c);
-void		move_cursor_left(t_config *sh);
-void		move_cursor_right(t_config *sh);
-void		process_arrow_up(t_config *sh, t_history *hist);
-void		process_arrow_down(t_config *sh, t_history *hist);
-
-/*** ctrl_keys ***/
-
-t_bool		process_ctrl_key(t_data *data, t_config *sh, t_history *hst, int c);
-void		process_ctrl_d(t_data *data, t_config *sh);
-t_bool		process_ctrl_c(t_config *sh);
-void		process_ctrl_l(t_config *sh, t_data *data);
-void		process_ctrl_u(t_config *sh, t_history *hist);
-
-void		setup_prompt(char **input, t_data *data);
-int			read_key(t_config *sh);
-char		*heredoc_process_keypress(
-				t_data *data, t_config *sh, t_history *hist, char *delimiter);
-char		*shell_process_keypress(
-				t_data *data, t_config *sh, t_history *hist);
-int			get_pos_cursor(int *cx, int *cy);
-
-/*** init ***/
-
-void		init_shell_values(t_config *sh);
-void		init_shell(t_config *sh);
-t_config	*init_config(char **envp);
-t_history	*init_history(void);
-
-/*** lists ***/
-
-t_history	*new_cmd(char *cmd, int num);
-t_history	*getlast(t_history *hist);
-void		add_cmd(t_history **hist, t_history *new);
-
-char		*find_cmd_history(t_history *hist, int h_num);
-void		print_history(t_history *hist);
-void		clear_hist(t_history *hist, int search);
-
-/*** utils ***/
-
-int			ctrl_key(int k);
-void		clear_prompt(int cx, int len);
-t_bool		already_exists(char *var, t_env *env);
 
 /*** exit ***/
 
 void		exit_error(const char *s, t_config *sh);
+void		free_env(t_env *g_env);
+void		free_split(char **args);
 void		exit_free(t_config *sh, t_history *hist);
 void		free_history(t_history *hist);
 
---
+++ inc/shell.h
@@ -0,0 +1,94 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   shell.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lraffin <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/22 12:17:46 by lraffin           #+#    #+#             */
+/*   Updated: 2021/11/22 12:24:47 by lraffin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SHELL_H
+# define SHELL_H
+
+# include "struct.h"
+# include "enum.h"
+
+/*** shell ***/
+
+void		setup_prompt(char **input, t_data *data);
+void		update_input(t_config *sh, t_history *hist, int c);
+char		*send_cmd(t_config *sh);
+
+/*** init ***/
+
+void		init_shell_values(t_config *sh);
+void		init_shell(t_config *sh);
+t_config	*init_config(char **envp);
+t_history	*init_history(void);
+
+/*** raw ***/
+
+void		disable_raw_mode(t_config *sh);
+t_bool		enable_raw_mode(t_config *sh);
+void		enable_heredoc_raw_mode(t_config *sh);
+void		init_shell(t_config *sh);
+
+/*** read input ***/
+
+int			read_key(t_config *sh);
+int			read_escape_seq(void);
+int			read_arrow_key(char c);
+int			read_key(t_config *sh);
+char		*shell_process_keypress(
+				t_data *data, t_config *sh, t_history *hist);
+char		*heredoc_process_keypress(
+				t_data *data, t_config *sh, t_history *hist, char *delimiter);
+
+/*** edit input ***/
+
+void		insert_char_history(t_history *hist, int cx, char c, int search);
+char		*insert_char(char *current, int cx, char c);
+
+void		erase_char(char *s, int cx);
+void		erase_char_history(t_history *hist, int cx, int search);
+void		erase_cmd_history(char *input, t_history *history, int search);
+
+/*** keys ***/
+
+void		erase_char_current(t_config *sh);
+void		erase_char_input(t_config *sh, t_history *hist);
+void		process_del_key(t_config *sh, t_history *hist);
+void		process_backspace_key(t_config *sh, t_history *hist);
+
+void		process_tab_key(t_config *sh);
+
+void		process_arrow_key(t_config *sh, t_history *hist, int c);
+void		move_cursor_left(t_config *sh);
+void		move_cursor_right(t_config *sh);
+void		process_arrow_up(t_config *sh, t_history *hist);
+void		process_arrow_down(t_config *sh, t_history *hist);
+
+t_bool		process_ctrl_key(t_data *data, t_config *sh, t_history *hst, int c);
+void		process_ctrl_d(t_data *data, t_config *sh);
+t_bool		process_ctrl_c(t_config *sh);
+void		process_ctrl_l(t_config *sh, t_data *data);
+void		process_ctrl_u(t_config *sh, t_history *hist);
+
+/*** lists ***/
+
+t_history	*new_cmd(char *cmd, int num);
+t_history	*getlast(t_history *hist);
+void		add_cmd(t_history **hist, t_history *new);
+char		*find_cmd_history(t_history *hist, int h_num);
+void		print_history(t_history *hist);
+void		clear_hist(t_history *hist, int search);
+
+/*** utils ***/
+
+int			ctrl_key(int k);
+void		clear_prompt(int cx, int len);
+
+#endif
