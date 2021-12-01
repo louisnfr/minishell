b9e0d5bfdfd6b9578e3fd3fa77c5c4e56252d1eb
Louis Raffin <louis.raffin@gmail.com>
2021-12-01 01:09:26 +0100

    minishell

--- src/env/create.c
+++ src/env/create.c
@@ -6,7 +6,7 @@
 /*   By: lraffin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/29 19:47:13 by lraffin           #+#    #+#             */
-/*   Updated: 2021/11/30 23:44:30 by lraffin          ###   ########.fr       */
+/*   Updated: 2021/12/01 01:08:46 by lraffin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,25 @@ static void	update_env(t_data *data, t_env *env)
 	tmp = safe_itoa(i, data);
 	set_env("SHLVL", tmp, env, data);
 	free(tmp);
+}
+
+static t_env	*dup_env_export(char **envp, t_data *data)
+{
+	t_env	*g_env;
+	char	**var;
+	int		i;
+
+	g_env = NULL;
+	i = -1;
+	while (envp[++i])
+	{
+		var = safe_split_on_first(envp[i], '=', data);
+		if (!ft_strcmp(var[0], "_"))
+			continue ;
+		add_var(&g_env, new_var(var[0], var[1], 1));
+		free_split(var);
+	}
+	return (g_env);
 }
 
 static t_env	*dup_env(char **envp, t_data *data)
@@ -46,7 +65,7 @@ static t_env	*dup_env(char **envp, t_data *data)
 void	create_update_env(char **envp, t_data *data)
 {
 	data->env = dup_env(envp, data);
-	data->export = dup_env(envp, data);
+	data->export = dup_env_export(envp, data);
 	update_env(data, data->env);
 	update_env(data, data->export);
 }
