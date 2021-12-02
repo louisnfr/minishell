a6d5cc13c95f708819b52a30d80495209ec7e41e
louisnfr <louis.raffin@gmail.com>
2021-12-02 18:21:33 +0100

    minishell

--- src/builtin/cd.c
+++ src/builtin/cd.c
@@ -6,7 +6,7 @@
 /*   By: lraffin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/28 15:03:41 by lraffin           #+#    #+#             */
-/*   Updated: 2021/12/02 17:32:52 by lraffin          ###   ########.fr       */
+/*   Updated: 2021/12/02 18:21:05 by lraffin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,7 +54,11 @@ t_bool	ch_dir(char *dir, char *error_msg, t_cmd *cmd_list)
 
 	ret = -1;
 	if (dir)
+	{
+		if (!ft_strlen(dir))
+			return (EXIT_SUCCESS);
 		ret = chdir(dir);
+	}
 	if (ret < 0)
 	{
 		if (error_msg)
