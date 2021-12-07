eb0f8e9ca8473c862bb7ce6fc4ce9e2a11dfaf60
louisnfr <louis.raffin@gmail.com>
2021-12-07 15:47:38 +0100

    minishell

--- src/builtin/env.c
+++ src/builtin/env.c
@@ -6,7 +6,7 @@
 /*   By: lraffin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/28 16:28:32 by lraffin           #+#    #+#             */
-/*   Updated: 2021/11/29 18:55:01 by lraffin          ###   ########.fr       */
+/*   Updated: 2021/12/07 15:08:36 by lraffin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,12 @@
 t_bool	exec_env(t_cmd *cmd_list, t_data *data)
 {
 	if (cmd_list->args)
-		return (EXIT_FAILURE);
+	{
+		ft_putstr_fd("env: '", STDERR_FILENO);
+		ft_putstr_fd(cmd_list->args[0], STDERR_FILENO);
+		ft_putstr_fd("': No such file or directory\n", STDERR_FILENO);
+		return (127);
+	}
 	print_env(data->env, cmd_list);
 	return (EXIT_SUCCESS);
 }
